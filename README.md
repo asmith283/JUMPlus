d339191ffde144f48ca2aeef3b9a19bc27fd8819
asmith283 <asmith41234@gmail.com>
2023-04-09 14:51:02 -0700

    new extensions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JUMPlus
+# dataCleanning.ipynb for the data cleaning and visualization
+- extensions
+- currency conversion
+
--- project1/ETL.py
+++ project1/ETL.py
@@ -1,10 +1,11 @@
 import pandas as pd
-
+from forex_python.converter import CurrencyRates
 
 def dataCleaner(df):
     df = df.dropna(how='all').drop('sofifa_id', axis=1) #drops complete rows that contain all null #i am also not doing normalization for the table
     df = df.rename(columns={'wage_eur': "wage_usd", "value_eur": "value_usd", "release_clause_eur" : "release_clause_usd"}) #changes eur to usd
     df[["wage_usd", "value_usd", "release_clause_usd"]] = df[["wage_usd", "value_usd", "release_clause_usd"]] * 1.20 #converts to usd
+    #df[["wage_usd", "value_usd", "release_clause_usd"]] = df[["wage_usd", "value_usd", "release_clause_usd"]].apply(lambda x: CurrencyRates().convert('USD', 'EUR', x))
     #changes all float columns to int
     df[["wage_usd", "value_usd", "release_clause_usd", 'team_jersey_number', 'contract_valid_until','nation_jersey_number', 'pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic', 'gk_diving', 'gk_handling', 'gk_kicking', 'gk_reflexes', 'gk_speed', 'gk_positioning' ]] = df[["wage_usd", "value_usd", "release_clause_usd", 'team_jersey_number', 'contract_valid_until','nation_jersey_number', 'pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic', 'gk_diving', 'gk_handling', 'gk_kicking', 'gk_reflexes', 'gk_speed', 'gk_positioning' ]].astype('Int64')
     df['body_type'] =  df['body_type'].replace(['Akinfenwa', 'C. Ronaldo', 'Courtois', 'Messi', 'Neymar', 'PLAYER_BODY_TYPE_25', 'Shaqiri'], 'Unique') #all incorrect body types changed to unique
